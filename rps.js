47da38d2c07c0902294e50327e33fbf5cba91f03
Biezmor <217460976+Biezmor@users.noreply.github.com>
2026-03-24 14:26:19 +0100

    Add step 5: Write the logic to play a single round Not quite right yet, to be changed

--- rps.js
+++ rps.js
@@ -1,35 +1,72 @@
-// Make the computer generate one of 3 string values
-// rock or paper or scissors
-//make a number between 1 and 10 OK
-// narrow down the number to 1-3 ?
-// return a number between 1 and 3
-// bind 1, 2, 3 to rock, paper, scissors
-//
 
-
-
-
-function getComputerChoice (rock, paper, scissors) {
- let result = (Math.ceil(Math.random() * 3));
- if (result === 1) {
+function getComputerChoice () {
+ let computerChoice = (Math.ceil(Math.random() * 3));
+ if (computerChoice === 1) {
     return "rock";
- } else if (result === 2) {
+ } else if (computerChoice === 2) {
     return "paper";
- } else if (result === 3) {
+ } else if (computerChoice === 3) {
     return "scissors";
- }
+ };
 }
-console.log(getComputerChoice());
 
-function getHumanChoice (rock, paper, scissors) {
-    let answer = prompt("What will you choose?", "");
-if (answer == "rock") {
+
+function getHumanChoice () {
+    let humanChoice = prompt("What will you choose?", "");
+if (humanChoice == "rock") {
     return "rock";
-} else if (answer == "paper") {
+} else if (humanChoice == "paper") {
     return "paper";
-} else if (answer == "scissors") {
+} else if (humanChoice == "scissors") {
     return "scissors";
 };
 }
 
-console.log(getHumanChoice());
+
+
+let humanScore = 0;
+let computerScore = 0;
+
+
+
+function playRound (humanChoice, computerChoice) {
+if (humanChoice === computerChoice) {
+    console.log( "Tie!");
+
+} else if (humanChoice == "rock" && computerChoice == "paper") {
+    ++computerScore;
+    console.log("You lose! Paper beats Rock");
+    
+} else if (humanChoice == "rock" && computerChoice == "scissors") {
+    ++humanScore;
+    console.log("You win! Rock beats Scissors");
+   
+} else if (humanChoice == "paper" && computerChoice == "rock") {
+    ++humanScore;
+    console.log("You win! Paper beats Rock");
+
+} else if (humanChoice == "paper" && computerChoice == "scissors") {
+    ++computerScore;
+    console.log("You lose! Scissors beat Paper");
+     
+} else if (humanChoice == "scissors" && computerChoice == "paper") {
+    ++humanScore;
+    console.log("You win! Scissors beat Paper");
+    
+} else if (humanChoice == "scissors" && computerChoice == "rock") {
+    ++computerScore;
+    console.log("You lose! Rock beats Scissors");
+};
+}
+
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+//console.log(getHumanChoice());
+//console.log(getComputerChoice());
+playRound(humanSelection, computerSelection);
+
+console.log(humanScore);
+console.log(computerScore);
+
