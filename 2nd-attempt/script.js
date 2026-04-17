46575a534ec5f5cbc46e736eab1915c491b5798d
Biezmor <217460976+Biezmor@users.noreply.github.com>
2026-04-17 14:31:12 +0200

    Add a simple UI to make the game display buttons, current scores and declare the winner after a player reaches 5 points.Beyond the scope of the Project Odin exercise, prevent the game from being played further once a player reaches 5 points, add a "play again" button that resets the game. Also totally forget to commit until the very end... bravo me -_-

--- 2nd-attempt/script.js
+++ 2nd-attempt/script.js
@@ -1,68 +1,93 @@
 const arr = ["rock", "paper", "scissors"];
+const btns = document.querySelectorAll(".btn");  
+const buttonBox = document.querySelector("#button-box");
+
 
 function getComputerChoice () {
     let index = (Math.floor(Math.random() * (arr.length)));
     return arr[index];
 };
 
-function getHumanChoice () {
-let answer = prompt("What will you choose?", "");
-
-for (let i = 0; i < arr.length; i++) {
-    if (answer.toLocaleLowerCase() === arr[i]) return arr[i]
-};
-}
-
 function playGame () {
+    let humanScore = 0;
+    let computerScore = 0;
     
-let humanScore = 0;
-let computerScore = 0;
-
-
-function playRound (humanChoice, computerChoice) {
-
-    
-
-    if (humanChoice === computerChoice) {
-        console.log (`Tie! You both selected ${humanChoice}!`)
-
-    } else if ((
-            humanChoice === arr[0] && computerChoice === arr[1])
-        || (humanChoice === arr[1] && computerChoice === arr[2])
-        || (humanChoice === arr[2] && computerChoice === arr[0])) {
-        ++computerScore
-        console.log (`You lose! ${computerChoice.at(0).toLocaleUpperCase() + computerChoice.slice(1)} beats ${humanChoice}!`);
+    function playRound (humanChoice, computerChoice) {
+        const results = document.createElement("div");
+        results.classList.add("resultBox");
         
-
-    } else if ((
-            computerChoice === arr[0] && humanChoice === arr[1])
-        || (computerChoice === arr[1] && humanChoice === arr[2])
-        || (computerChoice === arr[2] && humanChoice === arr[0])) {
-        ++humanScore
-        console.log (`You win! ${humanChoice.at(0).toLocaleUpperCase() + humanChoice.slice(1)} beats ${computerChoice}!`);
-       } else {
-        console.log('What the heck is that?')
-       };}
-
-
-
-for (let i = 0; i < 5; i++) {
-
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-playRound(humanSelection, computerSelection);
-}
-
-
-
-
-if (humanScore > computerScore) {
-    console.log ("YOU WON!")
-} else if (humanScore < computerScore) {
-    console.log("You lost ;c")
-} else if (humanScore === computerScore) {
-    console.log ("It's a tie!")
-}
-};
-
+        if (humanScore >= 5 || computerScore >= 5) return;
+    
+        if (humanChoice === computerChoice) {
+            results.textContent = `Tie! You both selected ${humanChoice}!`;
+           
+    
+        } else if ((
+                humanChoice === arr[0] && computerChoice === arr[1])
+            || (humanChoice === arr[1] && computerChoice === arr[2])
+            || (humanChoice === arr[2] && computerChoice === arr[0])) {
+            computerScore++
+            results.textContent = `You lose! ${computerChoice.at(0).toLocaleUpperCase() + computerChoice.slice(1)} beats ${humanChoice}!`;
+            
+    
+        } else if ((
+                computerChoice === arr[0] && humanChoice === arr[1])
+            || (computerChoice === arr[1] && humanChoice === arr[2])
+            || (computerChoice === arr[2] && humanChoice === arr[0])) {
+            humanScore++
+            results.textContent = `You win! ${humanChoice.at(0).toLocaleUpperCase() + humanChoice.slice(1)} beats ${computerChoice}!`;
+            
+    
+           } else {
+            results.textContent = 'What the heck is that?'
+           };
+           buttonBox.appendChild(results);
+           const score = document.createTextNode(` So far the score is ${humanScore} : ${computerScore}`)
+           results.appendChild(score);
+           
+    
+         if (humanScore === 5 || computerScore === 5) {
+            const finalWinner = document.createElement("p");
+    
+                if (humanScore > computerScore) {
+                    finalWinner.textContent = `YOU WON! Humanity ${humanScore} : Computers ${computerScore}`
+    
+                } else if (humanScore < computerScore) {
+                    finalWinner.textContent = `YOU LOST! Humanity ${humanScore} : Computers ${computerScore}`
+                
+                } else if (humanScore === computerScore) {
+                    finalWinner.textContent = `IT'S A TIE! Humanity ${humanScore} : Computers ${computerScore}`
+            };
+            results.appendChild(finalWinner);
+            
+            const playAgain = document.createElement("p");
+            playAgain.textContent = "Wanna play again?";
+            finalWinner.appendChild(playAgain);
+    
+            const againButton = document.createElement("button");
+            againButton.textContent = "Play again";
+             
+            againButton.addEventListener("click", function () {
+                //window.location.reload();
+                humanScore = 0;
+                computerScore = 0;
+                const reset = document.querySelectorAll(".resultBox");
+                reset.forEach((item)=> item.remove());
+            });
+         
+            playAgain.appendChild(againButton);
+        };
+        };
+        
+        btns.forEach((btn) => {
+        btn.addEventListener("click", () => {
+            const humanSelection = btn.textContent;
+            const computerSelection = getComputerChoice();
+            playRound(humanSelection, computerSelection);
+        })
+    })
+    };
 playGame()
+
+
+
